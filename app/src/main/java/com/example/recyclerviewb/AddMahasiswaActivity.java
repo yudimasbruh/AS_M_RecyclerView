1996c2709f2b6e65875462fadf93660f9729cf74
yudi muttaqin <yudimuttaqin33@gmail.com>
2022-05-23 11:20:01 +0700

    Update 23 May 2022

---
+++ app/src/main/java/com/example/recyclerviewb/AddMahasiswaActivity.java
@@ -0,0 +1,14 @@
+package com.example.recyclerviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMahasiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mahasiswa);
+    }
+}
